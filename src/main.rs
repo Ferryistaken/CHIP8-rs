42584bfdc66621903a684b793325852a6c331f8a
Ferry <alessandroferry@protonmail.com>
2020-09-06 21:19:07 +0200

    added chip8 struct

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+struct Chip8 {
+    register: [u8; 16],
+    memory: [u8; 4096],
+    index: u16,
+    program_counter: u16,
+    stack: [u16; 16],
+    stack_pointer: u8,
+    delay_timer: u8,
+    sound_timer: u8,
+    keypad: [u8; 16],
+    video: [[u32; 64] ; 32],
+    op_code: u16
+}
+
+
+fn main() {
+
+}
