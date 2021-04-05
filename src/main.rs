52aec3e2727701336632e6dab9d31cbe8a71570e
Ferry <alessandro.ferrari.2004@gmail.com>
2021-04-05 16:14:22 -0400

    added parsing from command line

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use structopt::StructOpt;
 
 mod chip8;
 use chip8::Chip8;
@@ -6,15 +7,34 @@ use std::{thread, time};
 
 use std::time::Duration;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Example", about = "CHIP8-rs options")]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Set speed
+    // we don't want to name it "speed", need to look smart
+    #[structopt(short = "c", long = "clock", default_value = "10")]
+    speed: f64,
+
+    /// Input file
+    #[structopt(short = "r", long = "rom", parse(from_os_str))]
+    rom: PathBuf,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+
+    println!("{:?}", opt);
+
     let mut chip8: Chip8 = Chip8::new();
     //chip8.debug();
 
     // TODO: make it so that the file is taken as a positional argument
-    chip8.load_rom(PathBuf::from(
-        "roms/test_opcode.ch8",
-    ));
+    chip8.load_rom(opt.rom);
 
 
     loop {
