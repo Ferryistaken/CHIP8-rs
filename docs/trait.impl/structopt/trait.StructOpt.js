32dfd36d673f11b98313fd9ba735e395c1424883
Ferry <alessandro.ferrari.2004@gmail.com>
2024-04-01 11:36:33 -0400

    updated docs

--- docs/trait.impl/structopt/trait.StructOpt.js
+++ docs/trait.impl/structopt/trait.StructOpt.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"chip8_rs":[["impl StructOpt for <a class=\"struct\" href=\"chip8_rs/struct.Opt.html\" title=\"struct chip8_rs::Opt\">Opt</a>"]]
+"chip8_rs":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"chip8_rs/struct.Opt.html\" title=\"struct chip8_rs::Opt\">Opt</a>"]],
+"structopt":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ target/doc/strsim/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["StrSimError"],"fn":["damerau_levenshtein","hamming","jaro","jaro_winkler","levenshtein","normalized_damerau_levenshtein","normalized_levenshtein","osa_distance"],"type":["HammingResult"]};
