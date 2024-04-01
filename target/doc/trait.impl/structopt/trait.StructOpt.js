75146e3b899f8a9679527cc24f025bff5f09256a
Ferry <alessandro.ferrari.2004@gmail.com>
2024-04-01 11:37:06 -0400

    updated docs

--- target/doc/trait.impl/structopt/trait.StructOpt.js
+++ target/doc/trait.impl/structopt/trait.StructOpt.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"chip8_rs":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"chip8_rs/struct.Opt.html\" title=\"struct chip8_rs::Opt\">Opt</a>"]],
-"structopt":[]
+"chip8_rs":[["impl StructOpt for <a class=\"struct\" href=\"chip8_rs/struct.Opt.html\" title=\"struct chip8_rs::Opt\">Opt</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
